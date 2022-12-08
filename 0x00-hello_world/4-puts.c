78b45c6af293ddfea14e433061826b3df0d36ac0
allvo6377 <alvinpilot@gmail.com>
2022-12-08 12:27:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/**
+/*
  * main- Entry point
  *
  * Return: Always 0 (Success)
@@ -10,7 +10,6 @@ int main(void)
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
-
 		return (0);
 
 }
