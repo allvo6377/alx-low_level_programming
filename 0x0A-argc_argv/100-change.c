679ed294245a35c4605e113f99bf0f7c5bfaf3fc
allvo6377 <alvinpilot@gmail.com>
2023-01-09 23:55:44 +0000

    created the program 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * main - function
- * @argc: length of argv
- * @argv: number of argument
- * Return: Always 0
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
  */
+
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
-	int position, total, change, aux;
-	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	position = total = change = aux = 0;
-
+	/* number of arguments passed to your program is not exactly 1 */
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	total = atoi(argv[1]); /*Covert str to int*/
+	/* use atoi function to convert character numerics to intergers */
+	num = atoi(argv[1]);
+	result = 0;
 
-	if (total <= 0)
+	/* if the number passed as the argument is negative, print 0 */
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	/*Declaring While*/
-	while (coins[position] != '\0')
+	/* using a for loop to select ememts of the coins array */
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		if (total >= coins[position])
+		/* keep subtracting value at coin[j] till num < coin[j] */
+		while (num >= coins[j])
 		{
-			aux = (total / coins[position]);
-			change += aux;
-			total -= coins[position] * aux;
+			result++;
+			num -= coins[j];
 		}
-
-		position++;
 	}
-	printf("%d\n", change);
-
+	printf("%d\n", result);
 	return (0);
