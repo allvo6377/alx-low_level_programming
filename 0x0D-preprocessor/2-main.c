8b58d7e9ea169eeab373480909d246691fb104e5
allvo6377 <alvinpilot@gmail.com>
2023-01-17 21:35:43 +0000

    task two completed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,7 +3,8 @@
  * main - prints the name of file
  * Return: Always 0 (Success)
  */
-int main(void){
-	printf("File: %s\n", __FILE__);
-return 0;
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
