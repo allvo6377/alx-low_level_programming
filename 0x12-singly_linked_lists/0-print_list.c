8d93002218259a27b1a80eeaa49d99e2cf304ae6
allvo6377 <alvinpilot@gmail.com>
2023-01-30 19:47:37 +0000

    Quick fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
-*print_list - print string and length of string for each element of 'h'
-*@h: pointer to first element of list
-*Return: length of list
-*/
+ * print_list - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
 size_t print_list(const list_t *h)
 {
-	size_t size = 0;
+	int count = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%u] (nil)\n", h->len);
-			h = h->next;
-			++size;
-			continue;
+			printf("[0] (nil)\n");
 		}
-		printf("[%u] %s\n", h->len, h->str);
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
 		h = h->next;
-		++size;
 	}
-	return (size);
+	return (count);
 }
