2d8a85f039a745a601f502f0200034d1ec5ec700
allvo6377 <alvinpilot@gmail.com>
2023-04-07 21:24:14 +0000

    Hot fix!

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,3 +1,6 @@
+#include "hash_tables.h"
+
+
 /**
  * key_index - gives you the index of a key
  * @key: key
@@ -8,3 +11,4 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
+}
