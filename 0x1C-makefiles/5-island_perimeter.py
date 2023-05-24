5feab4097d32f568d06646f1023b0e3d20bb4af7
allvo6377 <alvinpilot@gmail.com>
2023-05-24 18:09:30 +0300

    Quick fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,11 +12,16 @@ def island_perimeter(grid):
     Returns:
         The perimeter of the Island
     """
+    rows = len(grid)
+    cols = len(grid[0])
     perimeter = 0
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            if grid[y][x] == 1:
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
                 perimeter += 4
-            elif y == 0 or y == len(grid) - 1 or x == len(grid[0]) - 1:
-                perimeter += 2
+                if row > 0 and grid[row-1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col-1] == 1:
+                    perimeter -= 2
     return perimeter
